9dd2d4fd3444cdaaa8146af7106a02b9c1b358b1
Sachsenspielt <sachsenspielt3@gmail.com>
2023-10-30 15:39:34 +0100

    feat(backend): Added prisma.js for Database Integration
    Release-As: 0.1.0

--- packages/backend/src/backend.ts
+++ packages/backend/src/backend.ts
@@ -2,15 +2,20 @@ import express from 'express'
 import log4js from 'log4js'
 import routeIndex from './routeIndex'
 import { sendResponse } from './helpers/response'
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
 const port = process.env.BACKEND_PORT || 3000
 const packageJson = require('../package.json')
 
+// Configure logging
 const logger = log4js.getLogger(packageJson.name)
 logger.level = 'debug'
 
 logger.info('Starting backend on port %d', port)
+
+// Configure Prisma
+const prisma = new PrismaClient()
 
 app.get('/', (_req, res) => {
   sendResponse(res, 200, packageJson.version)
@@ -24,6 +29,16 @@ app.use((_req, res) => {
   sendResponse(res, 404, 'Not found')
 })
 
-app.listen(3000, () => {
+app.listen(3000, async () => {
+  try {
+    // Test if everything is working
+    await prisma.$connect()
+    logger.info('Connected to database!')
+  } catch (error: any) {
+    logger.fatal(error.message)
+    logger.error('Error connecting to database, shutting down.', error.message)
+    process.exit(1)
+  }
+
   logger.info('Backend listening on http://localhost:%d', port)
 })
